be22e9948fbb1ce8383463aa080a9cd3e578f3d6
Gustavo Gonzalez <gustavo@martintechlabs.com>
2011-04-20 15:41:27 -0500

    Fixed issue related to open_id

--- lib/generators/sitebootstrapper/templates/omniauth.rb
+++ lib/generators/sitebootstrapper/templates/omniauth.rb
@@ -2,5 +2,5 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   provider :facebook, 'APP_ID', 'APP_SECRET'
-  provider :open_id. OpenID::Store::Filesystem.new('/tmp')
+  provider :google_apps, OpenID::Store::Filesystem.new('./tmp')
 end
